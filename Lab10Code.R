f955f6f7a4e002a91a924adf727cdf293de9beda
chooey101 <charleshooey@gmail.com>
2025-04-07 16:10:13 -0400

    part 2

--- Lab10Code.R
+++ Lab10Code.R
@@ -2,7 +2,7 @@ library(tidyverse)
 
 #Part 1: Basic Simulation
 
-sim.study <- rbinom(n=1000, size=10000, prob=0.39) #Random binom sample
+sim.study <- rbinom(n=10000, size=1000, prob=0.39) #Random binom sample
 
 data <- tibble(x = sim.study)
 plot <- ggplot(data=data, aes(x = x)) +
@@ -22,3 +22,23 @@ plot <- ggplot(data=data, aes(x = x)) +
   theme_minimal()
 
 #The graph is not skewed; the middle 95 percent of the graph as a range of 200
+
+#Part 2: Perform Resampling
+
+R <- 1000
+resamples <- tibble(p.hat = numeric(R))
+for( i in 1:R){
+curr.resample <- sample(x = data$x,
+                        size = nrow(data),
+                        replace = T)
+# compute the stat on the resample
+resamples$p.hat[i] <- mean(curr.resample)
+}
+
+ggplot(resamples) + 
+  geom_histogram(aes(x=p.hat, y=after_stat(density)),
+                 bins = 30, 
+                 color="grey")+
+  geom_hline(yintercept=0)+
+  xlab("p.hat")+
+  ylab("Density")
