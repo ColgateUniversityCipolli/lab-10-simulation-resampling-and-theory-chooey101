537ec281b20042dab66e7073f7aba842c380f82d
chooey101 <charleshooey@gmail.com>
2025-04-07 17:19:17 -0400

    part 3

--- Lab10Code.R
+++ Lab10Code.R
@@ -35,10 +35,38 @@ curr.resample <- sample(x = data$x,
 resamples$p.hat[i] <- mean(curr.resample)
 }
 
-ggplot(resamples) + 
-  geom_histogram(aes(x=p.hat, y=after_stat(density)),
-                 bins = 30, 
-                 color="grey")+
-  geom_hline(yintercept=0)+
-  xlab("p.hat")+
-  ylab("Density")
+resample.plot <- ggplot(resamples, aes(x = p.hat)) + 
+  geom_histogram(aes(y = after_stat(density)), binwidth = 0.01, color = "grey", fill = "white") +
+  geom_density(color = "red", linewidth = 1) +
+  geom_hline(yintercept = 0) +
+  labs(x = "p.hat", y = "Density")
+
+
+#Part 3: simulation over n and p
+stored_values <- tibble(
+  n = numeric(),
+  p = numeric(),
+  p_hat = numeric(),
+  moe = numeric()
+)
+
+# Parameter values
+n_values <- seq(100, 3000, by = 10)
+p_values <- seq(0.01, 0.99, by = 0.01)
+
+# Loop through and collect stats
+for (n in n_values) {
+  for (p in p_values) {
+    sim <- rbinom(n = 10000, size = n, prob = p) / n
+    moe <- (quantile(sim, 0.975) - quantile(sim, 0.025)) / 2
+    
+    temp_df <- tibble(
+      n = n,
+      p = p,
+      p_hat = mean(sim),
+      moe = moe
+    )
+    
+    stored_values <- rbind(stored_values, temp_df)
+  }
+}
